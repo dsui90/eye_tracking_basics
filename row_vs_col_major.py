1ebf871d844d451e801180242f192d9f37f81aaa
skdupham <skdupham@win.uni-due.de>
2025-04-28 21:41:48 +0200

    update

--- row_vs_col_major.py
+++ row_vs_col_major.py
@@ -1,23 +1,24 @@
 import numpy as np
 from time import time
 
-number_of_elements = 5000
+num_rows = 2
+num_cols = 5000000
 
-A = np.zeros((number_of_elements, number_of_elements))
-B = np.zeros((number_of_elements, number_of_elements))
+A = np.zeros((num_rows, num_cols))
+B = np.zeros((num_rows, num_cols))
 
 # Row-major order
 start_time = time()
-for i in range(number_of_elements):
-    for j in range(number_of_elements):
+for i in range(num_rows):
+    for j in range(num_cols):
         A[i, j] = i + j
 end_time = time()
 print(f"Row-major order loop time: {end_time - start_time:.2f} seconds")
 
 # Column-major order
 start_time = time()
-for j in range(number_of_elements):
-    for i in range(number_of_elements):
+for j in range(num_cols):
+    for i in range(num_rows):
         B[i, j] = i + j
 end_time = time()
 print(f"Column-major order loop time: {end_time - start_time:.2f} seconds")
